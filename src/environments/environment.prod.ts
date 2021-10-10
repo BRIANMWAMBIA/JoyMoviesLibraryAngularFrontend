781182bfcc22ea2f816f63e5346b743cb838d551
brian <brianmwambia3@gmail.com>
2021-10-10 14:17:22 +0300

    changed environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: false
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBoyTveBS-uaOIkq6nA9Wk9Fc-kCNiNKKM",
+    authDomain: "my-library-82274.firebaseapp.com",
+    databaseURL: "https://my-library-82274-default-rtdb.firebaseio.com",
+    projectId: "my-library-82274",
+    storageBucket: "my-library-82274.appspot.com",
+    messagingSenderId: "291171062815",
+    appId: "1:291171062815:web:d4649a510b8be18c24f12a",
+    measurementId: "G-Q1LMVFMJPP"
+  }
+
 };
